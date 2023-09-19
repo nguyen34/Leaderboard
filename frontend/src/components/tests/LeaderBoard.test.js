9d0a5ef8ed0a4b512b80952caa81f1c6477fdeee
nguyen34 <nguyenjohnson65@gmail.com>
2023-09-18 22:40:08 -0700

    Tests Written Up

--- frontend/src/components/tests/LeaderBoard.test.js
+++ frontend/src/components/tests/LeaderBoard.test.js
@@ -8,6 +8,40 @@ describe('LeaderBoard', () => {
     let store;
     let wrapper;
     let vuetify;
+    let mockFetchUsers = jest.fn().mockImplementation(() => Promise.resolve());
+    let mockDeleteUser = jest.fn().mockImplementation(() => Promise.resolve());
+    let mockIncrementUserScore = jest.fn().mockImplementation(() => Promise.resolve());
+    let mockDecrementUserScore = jest.fn().mockImplementation(() => Promise.resolve());
+    let mockUsers = [
+        {
+            id: 1,
+            name: 'Test User',
+            age: 20,
+            address: 'Test Address',
+            points: 0,
+        },
+        {
+            id: 2,
+            name: 'Test User 2',
+            age: 20,
+            address: 'Test Address 2',
+            points: 1,
+        },
+        {
+            id: 3,
+            name: 'Test User 3',
+            age: 20,
+            address: 'Test Address 3',
+            points: 2,
+        },
+        {   
+            id: 4,
+            name: 'Test User 4',
+            age: 20,
+            address: 'Test Address 4',
+            points: 3,
+        }
+    ];
     beforeEach(() => {
         vuetify = createVuetify();
         store = createStore({
@@ -15,10 +49,13 @@ describe('LeaderBoard', () => {
                 userLeaderboards: {
                     namespaced: true,
                     state: {
-                        users: [],
+                        users: mockUsers,
                     },
                     actions: {
-                        fetchUsers: jest.fn().mockImplementation(() => Promise.resolve()),
+                        fetchUsers: mockFetchUsers,
+                        deleteUser: mockDeleteUser,
+                        incrementUserScore: mockIncrementUserScore,
+                        decrementUserScore: mockDecrementUserScore,
                     },
                 },
             },
@@ -35,9 +72,88 @@ describe('LeaderBoard', () => {
         expect(wrapper.vm.showUserDetails).toBe(false);
         expect(wrapper.vm.selectedUser).toEqual(null);
         expect(wrapper.vm.searchFilter).toBe('');
-        expect(wrapper.vm.displayedUsers).toEqual([]);
+        // This should equal the reverse of mockUsers since it orders by points in descending order by default
+        expect(wrapper.vm.displayedUsers).toEqual(mockUsers.reverse());
         expect(wrapper.vm.sortOrder).toBe('desc');
         expect(wrapper.vm.sortHeader).toBe('points');
     });
+    it('getUsers should call fetchUsers', async () => {
+        await wrapper.vm.getUsers();
+        expect(mockFetchUsers).toHaveBeenCalled();
+    });
+    it('handleDelete should call deleteUser', async () => {
+        await wrapper.vm.handleDelete(1);
+        expect(mockDeleteUser).toHaveBeenCalled();
+    });
+    it('handleIncrementScore should call incrementUserScore', async () => {
+        const user = {
+            id: 1,
+            name: 'Test User',
+            age: 20,
+            address: 'Test Address',
+            points: 0,
+        };
+        await wrapper.vm.handleIncrement(user);
+        expect(mockIncrementUserScore).toHaveBeenCalled();
+    });
+    it('handleDecrementScore should call decrementUserScore', async () => {
+        const user = {
+            id: 1,
+            name: 'Test User',
+            age: 20,
+            address: 'Test Address',
+            points: 0,
+        };
+        await wrapper.vm.handleDecrement(user);
+        expect(mockDecrementUserScore).toHaveBeenCalled();
+    });
+    it('showUserDetailsDialog should toggle user details dialog', () => { 
+        expect(wrapper.vm.showUserDetails).toBe(false);
+        expect(wrapper.vm.selectedUser).toEqual(null); 
+        const user = {
+            id: 1,
+            name: 'Test User',
+            age: 20,
+            address: 'Test Address',
+            points: 0,
+        };
+        wrapper.vm.showUserDetailsDialog(user);
+        expect(wrapper.vm.showUserDetails).toBe(true);
+        expect(wrapper.vm.selectedUser).toEqual(user);
+    });
+    it('filterAndSortUsers should filter and sort according to the provided parameters', () => {
+        expect(wrapper.vm.displayedUsers).toEqual(mockUsers.reverse());
+        wrapper.vm.filterAndSortUsers('name', 'asc');
+        expect(wrapper.vm.displayedUsers).toEqual(mockUsers);
+        wrapper.vm.filterAndSortUsers('name', 'desc');
+        expect(wrapper.vm.displayedUsers).toEqual(mockUsers.reverse());
+        wrapper.vm.filterAndSortUsers('points', 'asc');
+        expect(wrapper.vm.displayedUsers).toEqual(mockUsers);
 
+        // Now with searchFilter set to something
+        wrapper.vm.searchFilter = 'Test User 2';
+        wrapper.vm.filterAndSortUsers('name', 'asc');
+        expect(wrapper.vm.displayedUsers).toEqual([mockUsers[1]]);
+    });
+    it('toggleNameSort should change the sortOrder and sortHeader based off its current values', () => {
+        expect(wrapper.vm.sortOrder).toBe('desc');
+        expect(wrapper.vm.sortHeader).toBe('points');
+        wrapper.vm.toggleNameSort();
+        expect(wrapper.vm.sortOrder).toBe('asc');
+        expect(wrapper.vm.sortHeader).toBe('name');
+        wrapper.vm.toggleNameSort();
+        expect(wrapper.vm.sortOrder).toBe('desc');
+        expect(wrapper.vm.sortHeader).toBe('name');
+    });
+    it('togglePointsSort should change the sortOrder and sortHeader based off its current values', () => {
+        wrapper.vm.sortHeader = 'name';
+        expect(wrapper.vm.sortOrder).toBe('desc');
+        expect(wrapper.vm.sortHeader).toBe('name');
+        wrapper.vm.togglePointSort();
+        expect(wrapper.vm.sortOrder).toBe('asc');
+        expect(wrapper.vm.sortHeader).toBe('points');
+        wrapper.vm.togglePointSort();
+        expect(wrapper.vm.sortOrder).toBe('desc');
+        expect(wrapper.vm.sortHeader).toBe('points');
+    });
     });
